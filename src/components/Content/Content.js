5e756ed878a7a380c61dbd06ec0388d060090bba
Al-amin Yusuf <alaminyusuf131@gmail.com>
2020-10-03 22:35:58 +0100

    fixed issue with navbar

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// components
+import Img from '../../img/idea_bulb.jpg';
+
 import Header from '../Header';
 
 const Content = () => (
-  <Container>
+  <Container style={{ backgroundImage: `url(${Img})` }}>
     <Wrapper>
       <Header />
     </Wrapper>
@@ -14,16 +17,32 @@ const Content = () => (
 const Container = styled.div`
   font-family: Impact, Haettenschweiler, 'Arial Narrow Bold',
     sans-serif;
-  background-color: #757575;
-  color: #fff;
+  height: 100vh;
+  position: relative;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: top center;
+  width: 100%;
+  color: #ffc400;
+  &:after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    content: '';
+    /* opacity: 0.72; */
+    background: rgba(0, 0, 0, 0.35);
+    height: 100%;
+  }
 `;
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  /* color: #ffc400; */
   max-width: 90%;
   margin: 0 auto;
+  z-index: 10;
 `;
 
 export default Content;
